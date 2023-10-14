3be2fbbc2a55cce20e5b0254de39a7c9b14ee85f
Katteu <katheamari@gmail.com>
2023-10-15 00:05:01 +0800

    Modified Add Member to accept null

--- add_member.php
+++ add_member.php
@@ -6,27 +6,35 @@ $firstname = $_POST['firstName'];
 $lastname = $_POST['lastName'];
 $sex = $_POST['sex'];
 $email = $_POST['email'];
-$alt_email = $_POST['alt_email'];
+$alt_email = isset($_POST['alt_email']) ? $_POST['alt_email'] : NULL;
 $type = $_POST['type'];
-$stub_number = $_POST['stub_number'];
-$remarks = $_POST['remarks'];
+$stub_number = isset($_POST['stub_number']) ? $_POST['stub_number'] : NULL;
+$remarks = isset($_POST['remarks']) ? $_POST['remarks'] : NULL;
 
-$sql = "INSERT INTO `members`(`firstname`,`lastname`,`email`,`alt_email`,`sex`,`type`, `stub_number`, `remarks`) VALUES('$firstname', '$lastname', '$email','$alt_email','$sex','$type', '$stub_number', '$remarks')";
-$query = mysqli_query($con, $sql);
-if($query == true) 
-{
-    $data = array(
-        'status' => 'success',
-        'message' => 'Member added successfully',
-    );
-    echo json_encode($data);
-}
+$sql = "INSERT INTO `members`(`firstname`,`lastname`,`email`,`alt_email`,`sex`,`type`, `stub_number`, `remarks`) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
-else 
-{
+if ($stmt = mysqli_prepare($con, $sql)) {
+    mysqli_stmt_bind_param($stmt, "ssssssss", $firstname, $lastname, $email, $alt_email, $sex, $type, $stub_number, $remarks);
+
+    if (mysqli_stmt_execute($stmt)) {
+        $data = array(
+            'status' => 'success',
+            'message' => 'Member added successfully',
+        );
+        echo json_encode($data);
+    } else {
+        $data = array(
+            'status' => 'failed',
+            'message' => 'Error adding member',
+        );
+        echo json_encode($data);
+    }
+
+    mysqli_stmt_close($stmt);
+} else {
     $data = array(
         'status' => 'failed',
-        'message' => 'Error adding member',
+        'message' => 'Error preparing the SQL statement',
     );
     echo json_encode($data);
 }
